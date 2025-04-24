52185ffe8afc4e5b8e65953e191a3be63d8364fb
Rasmus Siemers <r.siemers@trustindialog.de>
2025-04-24 12:46:53 +0200

    added formatter

---
+++ lua/meek/plugins/formatter.lua
@@ -0,0 +1,37 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
+
+        conform.setup({
+            formatters_by_ft = {
+                javascript = { "prettierd" },
+                typescript = { "prettierd" },
+                javascriptreact = { "prettierd" },
+                typescriptreact = { "prettierd" },
+                css = { "prettierd" },
+                html = { "prettierd" },
+                json = { "prettierd" },
+                yaml = { "prettierd" },
+                lua = { "stylua" },
+                python = { "isort", "black" },
+            },
+            format_on_save = {
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            },
+        })
+
+        local keymap = vim.keymap
+
+        keymap.set("n", "<leader>pf", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            })
+        end, { desc = "Format with Prettier" })
+    end,
+}
--- lua/meek/plugins/terminal.lua
+++ lua/meek/plugins/terminal.lua
@@ -19,6 +19,6 @@ return {
 
         local keymap = vim.keymap
 
-            keymap.set({"t", "n"}, "<leader>tm", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" })
+            keymap.set({"t", "n"}, "<M-t>", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" })
     end,
 }
